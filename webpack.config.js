97e9c1cb0757b763af409b76fdb3302cb21173ff
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-06-12 00:27:20 +0800

    move

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: [
-    './src/entry.js'
+    './entry.js'
   ],
   output: {
     path: path.resolve(__dirname, 'build'),
